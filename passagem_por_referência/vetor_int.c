bb1f0102f8b9c26cd85e979d6392b2ef2b6f58e2
Arthur Hernandez <57412723+arthurhernandezp@users.noreply.github.com>
2022-12-03 23:54:05 -0300

    Create vetor_int.c

---
+++ passagem_por_referência/vetor_int.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void lerVet(int vet[]){
+    for(int i = 0; i < sizeof(vet/sizeof(int);i++){
+        printf("Digite o %d numero: ",i);
+        scanf("%d",vet[i]);
+    }
+}
+        
+int main(){
+    int vet[10]
+    letVet(vet);
+    return 0;
+}
